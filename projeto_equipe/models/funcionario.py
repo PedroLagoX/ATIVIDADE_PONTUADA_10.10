efc27f77fbe903fd17106793113bc0f368ba9c0e
Juliocesarbonfim <159816707+Juliocesarsenai@users.noreply.github.com>
2024-09-30 12:51:08 +0000

    Salvar

---
+++ projeto_equipe/models/funcionario.py
@@ -0,0 +1,19 @@
+import os 
+from projeto_equipe.models.Endereco import Endereco
+from abc import ABC
+
+
+os.system("cls||clear")
+
+class Funcionario(ABC):
+    def __init__(self,nome:str,telefone:str,email:str,endereco:Endereco) -> None:
+        self.nome=nome
+        self.telefone=telefone
+        self.email=email
+        self.endereco=endereco
+    
+    def __str__(self) -> str:
+        return (f"\nNome: {self.nome}"
+                f"\nTelefone: {self.telefone}"
+                f"\nEmail:{self.email}"
+                f"\nEndereco:{self.endereco}")
