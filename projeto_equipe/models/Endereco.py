e91e6c5fd403ccfd0463c2b9213b615b811088be
Caio Pinheiro <86423541+caiofpinheiro@users.noreply.github.com>
2024-09-30 12:30:06 +0000

    Endereco

---
+++ projeto_equipe/models/Endereco.py
@@ -0,0 +1,16 @@
+class Endereco:
+    def __init__(self, logradouro: str, numero: str, complemento: str, cep: str, cidade: str) -> None:
+        self.logradouro = logradouro
+        self.numero = numero
+        self.complemento = complemento
+        self.cep = cep
+        self.cidade = cidade
+
+    def __str__(self) -> str:
+        return (
+            f"\nLogradouro: {self.logradouro}"
+            f"Número: {self.numero}"
+            f"Complemento: {self.complemento}"
+            f"CEP: {self.cep}"
+            f"Cidade: {self.cidade}"
+        )
