c45f7239a2c00376062361abe5c476cf76424ab5
Juliocesarbonfim <159816707+Juliocesarsenai@users.noreply.github.com>
2024-10-07 13:55:38 +0000

    Salvar

--- projeto_equipe/tests/test_engenheiro.py
+++ projeto_equipe/tests/test_engenheiro.py
@@ -8,19 +8,32 @@ endereco2=Endereco("Rua B","41","600","321021-213","Salvador")
 
 @pytest.fixture
 def engenheiro_valido():
-    return Engenheiro(1234,2123,122,endereco2,21331)
+    return Engenheiro("Julio Cesar","7198291283","juliocesar@gmail.com",endereco2,"67732421")
 
 
 def test_criar_engenheiro_nome(engenheiro_valido):
-    assert engenheiro_valido.nome== "Julio Cesar"
-
+    try:
+        assert engenheiro_valido.nome== "Julio Cesar"
+    except AssertionError as erro:
+        print(f"Erro de Asserção:{erro}")
+        raise
 def test_criar_engenheiro_telefone(engenheiro_valido):
-    assert engenheiro_valido.telefone== "7198291283"
-
+    try:
+        assert engenheiro_valido.telefone== "7198291283"
+    except AssertionError as erro:
+        print(f"Erro de Asserção:{erro}")
+        raise
 def test_criar_engenheiro_email(engenheiro_valido):
-    assert engenheiro_valido.email== "juliocesar@gmail.com"
-
+    try:
+        assert engenheiro_valido.email== "juliocesar@gmail.com"
+    except AssertionError as erro:
+        print(f"Erro de Asserção:{erro}")
+        raise
 def test_criar_engenheiro_crea(engenheiro_valido):
-    assert engenheiro_valido.crea== "67732421"
-
+    try:
+        assert engenheiro_valido.crea== "67732421"
+    except AssertionError as erro:
+        print(f"Erro de Asserção:{erro}")
+    
+        raise
 
