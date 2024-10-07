bb1121533181cf9ba96e0ad2563198e38ddd1969
Pedro Lago <159453815+PedroLagoX@users.noreply.github.com>
2024-10-07 14:07:14 +0000

    salvr

--- projeto_equipe/models/medico.py
+++ projeto_equipe/models/medico.py
@@ -5,6 +5,46 @@ class Medico(Funcionario):
     def __init__(self, nome: str, telefone: str, email: str, endereco: Endereco, crm:str) -> None:
         super().__init__(nome, telefone, email, endereco)
         self.crm = crm
-    
+    def __verificar_nome(self,nome):
+        try:
+            if not nome:
+                raise ValueError("O nome não pode estar vazio")
+            if not isinstance(nome,str):
+                raise TypeError("O nome só pode ser uma string")
+        except(ValueError,TypeError) as erro:
+            print(erro)
+            
+        return nome
+
+    def __verificar_telefone(self,telefone):   
+        try:
+            if not telefone:
+                raise ValueError("O telefone não pode estar vazio")
+            if not isinstance(telefone,str):
+                raise TypeError("O telefone só pode ser uma string")
+        except(ValueError,TypeError) as erro:
+            print(erro)
+        return telefone
+    def __verificar_email(self,email):
+        try:
+            if not email:
+                raise ValueError("O email não pode estar vazio")
+            if not isinstance(email,str):
+                raise TypeError("O email só pode ser uma string")
+        except(ValueError,TypeError) as erro:
+            print(erro)
+        
+        return email
+   
+    def __verificar_crea(self,crea):
+        try:
+            if not crea:
+                raise ValueError("O crea não pode estar vazio")
+            if not isinstance(crea,str):
+                raise TypeError("O crea só pode ser uma string")
+        except(ValueError,TypeError) as erro:
+            print(erro)
+        return crea
+
     def __str__(self) -> str:
         return (f"{super().__str__()} \nCRM: {self.crm}")
--- projeto_equipe/tests/test_medico.py
+++ projeto_equipe/tests/test_medico.py
@@ -6,19 +6,32 @@ endereco3=Endereco("Rua C","40","500","123023-123","Salvador")
 
 @pytest.fixture
 def medico_valido():
-    return Medico("Caio Fernando","719828213","caiofernando@gmail.com",endereco3,"232302131")
-
+    return Medico("","719828213","caiofernando@gmail.com",endereco3,"232302131")
 
 def test_criar_medico_nome(medico_valido):
-    assert medico_valido.nome== "Caio Fernando"
+    try:
+        assert medico_valido.nome== "Caio Fernando"
+    except AssertionError as erro:
+        print(f"Erro de Asserção:{erro}")
+        raise
 
 def test_criar_medico_telefone(medico_valido):
-    assert medico_valido.telefone== "719828213"
+    try:
+        assert medico_valido.telefone== "719828213"
+    except AssertionError as erro:
+        print(f"Erro de Asserção: {erro}")
+        raise
 
 def test_criar_medico_email(medico_valido):
-    assert medico_valido.email== "caiofernando@gmail.com"
+    try:
+        assert medico_valido.email== "caiofernando@gmail.com"
+    except AssertionError as erro:
+        print(f"Erro de Asserção: {erro}")
+        raise
 
 def test_criar_medico_crm(medico_valido):
-    assert medico_valido.crm== "232302131"
-
-
+    try:
+        assert medico_valido.crm== "232302131"
+    except AssertionError as erro:
+        print(f"Erro de Asserção: {erro}")
+        raise
