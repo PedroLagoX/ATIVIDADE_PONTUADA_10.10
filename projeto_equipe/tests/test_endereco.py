f657540a4a3eb8ad8a9583dc08cae10aa12b8adf
Caio Pinheiro <86423541+caiofpinheiro@users.noreply.github.com>
2024-10-07 12:29:10 +0000

    salvar

--- projeto_equipe/tests/test_endereco.py
+++ projeto_equipe/tests/test_endereco.py
@@ -1,2 +1,12 @@
 import pytest
 from ..models.Endereco import Endereco
+from projeto_equipe.models.Endereco import Endereco
+
+@pytest.fixture
+
+def criar_endereco():
+    endereco_1 = Endereco("Rua A", "123", "ap0", "0000-000", "Salvador")
+    return endereco_1
+
+def test_criar_endereco_logradouro(criar_endereco):
+    assert criar_endereco.logradouro == "Rua A"
