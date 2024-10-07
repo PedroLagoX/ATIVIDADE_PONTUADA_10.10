7cf845d80f8eea64fd4a6d2a6a596faad7501bfb
Caio Pinheiro <86423541+caiofpinheiro@users.noreply.github.com>
2024-10-07 12:38:57 +0000

    salvar

--- projeto_equipe/tests/test_endereco.py
+++ projeto_equipe/tests/test_endereco.py
@@ -10,3 +10,15 @@ def criar_endereco():
 
 def test_criar_endereco_logradouro(criar_endereco):
     assert criar_endereco.logradouro == "Rua A"
+
+def test_criar_endereco_numero(criar_endereco):
+    assert criar_endereco.numero == "123"
+
+def test_criar_endereco_complemento(criar_endereco):
+    assert criar_endereco.complemento == "ap0"
+
+def test_criar_endereco_cep(criar_endereco):
+    assert criar_endereco.cep == "0000-000"
+
+def test_criar_endereco_cidade(criar_endereco):
+    assert criar_endereco.cidade == "Salvador"
